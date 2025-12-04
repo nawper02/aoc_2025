41b776c27bcd87bba9b9336c209999d2d0a8645b
Kin <kin.blandford@tealdrones.com>
2025-12-04 10:04:51 -0700

    day4 p1

---
+++ day4/build/CMakeFiles/P2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P2.dir/p2.cpp.o"
+  "CMakeFiles/P2.dir/p2.cpp.o.d"
+  "P2"
+  "P2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ day4/p1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <array>
+#include <utility>
+
+constexpr std::array<std::pair<int, int>, 8> directions = {{
+    {-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {1, 1}, {-1, 1}, {1, -1}
+}};
+
+bool lessThanFourNeighbors(int r, int c, const std::vector<std::string>& grid) {
+    int numNeighbors = 0;
+    for (auto [dr, dc] : directions) {
+        int nR = r + dr;
+        int nC = c + dc;
+        if (nR >= 0 && nR < grid.size() && nC >= 0 && nC < grid[0].length()) {
+            if (grid[nR][nC] == '@') {
+              numNeighbors++;
+              std::cout << "Checked (" << nR << "," << nC << ") for (" << r << "," << c << ") and incremented neighbor count" << std::endl;
+            }
+        }
+    }
+    std::cout << numNeighbors << " neighbors for (" << r << "," << c << ")" << std::endl;
+    return numNeighbors < 4;
+}
+
+int main()
+{
+  std::ifstream inputFile("../input.txt");
+  std::stringstream inputBuffer;
+  inputBuffer << inputFile.rdbuf();
+
+  std::string line;
+  std::vector<std::string> grid;
+  while (std::getline(inputBuffer, line))
+  {
+    grid.push_back(line);
+  }
+
+  int numRollsAccessible = 0;
+
+  for (int r = 0; r < grid.size(); r++)
+  {
+    for (int c = 0; c < grid[r].length(); c++)
+    {
+      if (grid[r][c] != '@') continue;
+      if (lessThanFourNeighbors(r,c,grid)) numRollsAccessible++;
+    }
+  }
+
+  std::cout << "Num rolls accessible: " << numRollsAccessible << std::endl;
+
+  // cleanup and exit
+  inputFile.close();
+  return 0;
+}
